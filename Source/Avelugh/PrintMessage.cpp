8f469de33bb64fe213fd7184ed4d05abb0577e54
Olviia <olviia.stroivans@gmail.com>
2024-07-12 00:41:38 +0100

    git commit

---
+++ Source/Avelugh/PrintMessage.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PrintMessage.h"
+
+/**
+ * Sets default values
+*/
+APrintMessage::APrintMessage()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+/**
+ * Called when the game starts or when spawned
+*/
+void APrintMessage::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("ave Lugh"));
+	
+}
+
+/**
+ * Called every frame
+ * @param DeltaTime Game time elapsed during last frame modified by the time dilation
+*/
+void APrintMessage::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
